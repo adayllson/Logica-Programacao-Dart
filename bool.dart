e20db790e792b72e2a60dd5652cc5ea9a7691d80
Adailson Almeida <adayllson.almeida@gmail.com>
2023-04-25 16:41:40 -0300

    boolean

---
+++ bool.dart
@@ -0,0 +1,15 @@
+void main() {
+  // 0 - false
+  // 1 - true
+  var a = "teste";
+  var b = "teste2";
+  print(a == b);
+
+  var isTrue = true;
+  var isFalse = false;
+
+  if (!isFalse) {
+    print("Entrou!");
+  }
+
+}
